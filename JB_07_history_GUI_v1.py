b07bbde19c750a8db95525dd42c54eebac3a6a22
CustodioJB <100053960+CustodioJB@users.noreply.github.com>
2024-02-26 10:02:04 +1300

    Created History/Export GUI

---
+++ JB_07_history_GUI_v1.py
@@ -0,0 +1,100 @@
+from tkinter import *
+from functools import partial # To prevent unwanted windows
+
+class Converter:
+
+    def __init__(self):
+        # common format for all buttons
+        # Arial size 14 bold, with white text
+        button_font = ("Arial", "12", "bold")
+        button_fg = "#FFFFFF"
+
+        # Five item list
+        # self.all_calculations = ['0 F° is -18 C°', '0 C° is 32 F°', '30 F° is -1 C°', '30 C° is 86 F°', '40 F° is 4 C°']
+
+        # Six Item list
+        self.all_calculations = ['0 F° is -18 C°', '0 C° is 32 F°', '30 F° is -1 C°', '30 C° is 86 F°', '40 F° is 4 C°']
+
+        # set up GUI frame
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        self.button_frame = Frame(padx=30, pady=30)
+        self.button_frame.grid(row=0)
+
+        self.to_history_button = Button(self.button_frame, text = "History / Export", bg ="#004C99", fg=button_fg, font=button_font, width=12,command=self.to_history)
+
+        self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
+        
+    def to_history(self):
+        HistoryExport(self)
+
+
+class HistoryExport:
+
+    def __init__(self, partner):
+
+        # setup dialogue box and background colour
+        self.history_box = Toplevel()
+        
+        # disable history button
+        partner.to_history_button.config(state=DISABLED)
+
+        # If users press cross at top, closes history and 'releases' history button
+        self.history_box.protocol('WM_DELETE_WINDOW', partial(self.close_history,partner))
+
+        self.history_frame = Frame(self.history_box, width=300, height=200)
+
+        self.history_frame.grid()
+
+        self.history_heading_label = Label(self.history_frame, text = "History / Export", font=("Arial","16","bold"))
+
+        self.history_heading_label.grid(row=0)
+
+        # History text and label
+        hist_text = "Below are your recent calculations showing 3 /3 calculations. All calculations are shown to the nearest degree"
+
+        self.hist_text_label = Label(self.history_frame, text=hist_text, justify="left", wraplength=300, padx=10, pady=10)
+
+        self.hist_text_label.grid(row=1)
+
+        self.all_calcs_label = Label(self.history_frame, text = "Calculations go here", padx= 10, pady = 10, bg = "#ffe6cc", width=40, justify="left")
+
+        self.all_calcs_label.grid(row=2)
+
+        # Instructions for Saving Files
+        save_text = "Either choose a custom file name (and push <Export>) or simply push <Export> to save your calculations in a text file. If the filename already exists, it will be overwritten!"
+        self.save_instructions_label = Label(self.history_frame, text=save_text, wraplength=300, justify="left", width=40, padx=10, pady=10)
+        self.save_instructions_label.grid(row=3)
+
+        # Filename entry widget, white background to start
+        self.filename_entry = Entry(self.history_frame, font = ("Arial", "14"), bg = "#ffffff", width = 25)
+        self.filename_entry.grid(row = 4, padx=10, pady=10)
+
+        self.filename_error_label = Label(self.history_frame, text = "File name error goes here", fg = "#9C0000", font = ("Arial", "12", "bold"))
+
+        self.filename_error_label.grid(row=5)
+
+        self.button_frame = Frame(self.history_frame)
+        self.button_frame.grid(row=6)
+
+        self.export_button = Button(self.button_frame, font = ("Arial", "12", "bold"), text="Export", bg = "#004C99", fg = "#FFFFFF", width = 12)
+
+        self.export_button.grid(row=0, column=0, padx=10, pady=10)
+
+        self.dismiss_button = Button(self.history_frame, font=("Arial", "12", "bold"), text="Dismiss", bg="#666666", fg = "#FFFFFF", width=12, command=partial(self.close_history,partner))
+
+        self.dismiss_button.grid(row=0, column=1, padx=10, pady=10)
+    
+    # closes history dialogue (used by button and x at top of dialogue)
+    def close_history(self, partner):
+        # Put history button back to nrmal...
+        partner.to_history_button.config(state=NORMAL)
+        self.history_box.destroy()
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
